1543e90eea6cbcc7ae765d90a249c197cfb3f6ad
NEXALO Dev <dev@nexalo.xyz>
2026-07-10 13:39:01 +0200

    Configure Capacitor for Android with www assets and GitHub Actions build workflow

---
+++ android/app/src/main/java/com/lasdivasmadrid/swingersphere/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lasdivasmadrid.swingersphere;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lasdivasmadrid.swingersphere',
+  appName: 'SwingerSphere',
+  webDir: 'www'
+};
+
+export default config;
