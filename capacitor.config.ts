01f6b3d6e1ecd33ad0cecca7a0e0ff6052a8ed65
NEXALO Dev <dev@nexalo.xyz>
2026-08-25 17:02:43 +0200

    Fix: convertido capacitor.config.ts a capacitor.config.json para evitar errores de compilacion en CI por falta de compilador typescript

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.lasdivasmadrid.swingersphere',
-  appName: 'SwingerSphere',
-  webDir: 'www'
-};
-
-export default config;
